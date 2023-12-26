c6604637c7ef2a7e3913f21debc04d0c23f99d5f
InHyeok Park <pih@InHyeoknoMacBook-Air.local>
2023-12-27 01:44:08 +0900

    make files

---
+++ news.js
@@ -0,0 +1,161 @@
+const apiKey = '15a6a2e3d8ed4f8884fa5d99e586d442';
+const apiUrl = `http://newsapi.org/v2/top-headlines?country=jp&apiKey=${apiKey}`;
+
+async function getNews() {
+    try {
+        const response = await fetch(apiUrl);
+        const data = await response.json();
+        displayNews(data.articles);
+    } catch (error) {
+        console.error('ニュースの取得に失敗しました:', error);
+    }
+}
+
+function displayNews(articles) {
+    const newsContainer = document.getElementById('news-container');
+    newsContainer.innerHTML = articles.map((article, index) => `
+        <div class="news-item">
+            <h1>記事 ${index + 1}</h1>
+            <img src="${article.urlToImage}" width="250" alt="ニューストプ画">
+            <h2><a href="${article.url}" target="_blank" class="news-link">${article.title}</a></h2>
+            <p>${article.description}</p>
+        </div>
+    `).join('');
+}
+getNews();
+
+
+
+// particles.jsの設定
+particlesJS("particles-js", {
+    "particles": {
+        "number": {
+            "value": 400,
+            "density": {
+                "enable": true,
+                "value_area": 800
+            }
+        },
+        "color": {
+            "value": "#ffffff"
+        },
+        "shape": {
+            "type": "circle",
+            "stroke": {
+                "width": 0,
+                "color": "#000000"
+            },
+            "polygon": {
+                "nb_sides": 7
+            },
+            "image": {
+                "src": "img/github.svg",
+                "width": 100,
+                "height": 100
+            }
+        },
+        "opacity": {
+            "value": 0.5,
+            "random": true,
+            "anim": {
+                "enable": false,
+                "speed": 1,
+                "opacity_min": 0.1,
+                "sync": false
+            }
+        },
+        "size": {
+            "value": 15.782983970406905,
+            "random": true,
+            "anim": {
+                "enable": false,
+                "speed": 40,
+                "size_min": 0.1,
+                "sync": false
+            }
+        },
+        "line_linked": {
+            "enable": false,
+            "distance": 500,
+            "color": "#ffffff",
+            "opacity": 0.4,
+            "width": 2
+        },
+        "move": {
+            "enable": true,
+            "speed": 6,
+            "direction": "bottom",
+            "random": false,
+            "straight": false,
+            "out_mode": "out",
+            "bounce": false,
+            "attract": {
+                "enable": false,
+                "rotateX": 600,
+                "rotateY": 1200
+            }
+        }
+    },
+    "interactivity": {
+        "detect_on": "window",
+        "events": {
+            "onhover": {
+                "enable": true,
+                "mode": "bubble"
+            },
+            "onclick": {
+                "enable": true,
+                "mode": "bubble"
+            },
+            "resize": true
+        },
+        "modes": {
+            "grab": {
+                "distance": 400,
+                "line_linked": {
+                    "opacity": 0.5
+                }
+            },
+            "bubble": {
+                "distance": 400,
+                "size": 4,
+                "duration": 0.3,
+                "opacity": 1,
+                "speed": 3
+            },
+            "repulse": {
+                "distance": 200,
+                "duration": 0.4
+            },
+            "push": {
+                "particles_nb": 4
+            },
+            "remove": {
+                "particles_nb": 2
+            }
+        }
+    },
+    "retina_detect": true
+});
+
+// stats.jsの設定
+var count_particles, stats, update;
+stats = new Stats;
+stats.setMode(0);
+stats.domElement.style.position = 'absolute';
+stats.domElement.style.left = '0px';
+stats.domElement.style.top = '0px';
+document.body.appendChild(stats.domElement);
+count_particles = document.querySelector('.js-count-particles');
+update = function () {
+    stats.begin();
+    stats.end();
+    if (window.pJSDom[0].pJS.particles && window.pJSDom[0].pJS.particles.array) {
+        count_particles.innerText = window.pJSDom[0].pJS.particles.array.length;
+    }
+}
+
+requestAnimationFrame(update); 
+
+
+
